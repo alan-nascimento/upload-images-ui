632714af8e899a185084ec8c43fd31f986db1b14
Alan <alan.silva.n@hotmail.com>
2019-08-06 14:19:26 -0300

    Adding react-dropzone and its styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import GlobalStyle from './styles/global';
 import { Container, Content } from './styles';
+import Upload from './components/Upload';
 
 export default class App extends Component {
   componentDidMount() {}
@@ -8,7 +9,9 @@ export default class App extends Component {
   render() {
     return (
       <Container>
-        <Content>Testing</Content>
+        <Content>
+          <Upload />
+        </Content>
         <GlobalStyle />
       </Container>
     );
--- src/components/Upload/index.js
+++ src/components/Upload/index.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
+import Dropzone from 'react-dropzone';
+import { DropContainer, UploadMessage } from './styles';
 
 export default class Upload extends Component {
   componentDidMount() {}
 
+  renderDragMessage = (isDragActive, isDragReject) => {
+    if (!isDragActive) {
+      return <UploadMessage>Drag your images here...</UploadMessage>;
+    }
+
+    if (isDragReject) {
+      return <UploadMessage type="error">This file is not supported</UploadMessage>;
+    }
+
+    return <UploadMessage type="success">Drop your images here</UploadMessage>;
+  };
+
   render() {
-    return <div />;
+    return (
+      <Dropzone accept="image/*" onDropAccepted={() => {}}>
+        {({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
+          <DropContainer
+            {...getRootProps()}
+            isDragActive={isDragActive}
+            isDragReject={isDragReject}
+          >
+            <input {...getInputProps()} />
+            {this.renderDragMessage(isDragActive, isDragReject)}
+          </DropContainer>
+        )}
+      </Dropzone>
+    );
   }
 }
